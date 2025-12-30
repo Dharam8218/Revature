d4cb09f2cd987eba80cc897939d53d29c73ce433
dharam8218 <dharamveersinghgtb@gmail.com>
2025-12-30 15:09:32 +0530

    new js file

--- JavaScript/Day 7/first.js
+++ JavaScript/Day 7/first.js
@@ -1,2 +1,23 @@
 var a = 10;
-console.log(a);
+let b = 20;
+const c = 30;
+
+function print() {
+  console.log(a);
+  a++;
+  console.log(a);
+
+  console.log(b);
+  b++;
+  console.log(b);
+
+  console.log(c);
+}
+
+print();
+
+let names = ["abc", "def", "ghi", "jkl", "mno"];
+for(let i=0;i<names.length;i++){
+    console.log(names[i]);
+}
+
