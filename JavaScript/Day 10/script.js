4afa06d5d9caf8640b69eae41d08bbead18e584d
dharam8218 <dharamveersinghgtb@gmail.com>
2026-01-04 22:52:20 +0530

    Day 10

---
+++ JavaScript/Day 10/script.js
@@ -0,0 +1,25 @@
+// console.log("Start");
+
+// setTimeout(()=>{
+//     console.log("Timeout");
+// },0);
+
+// Promise.resolve().then(()=>{
+//     console.log("Promise");
+// });
+
+// console.log("End");
+
+const arr = [2, 5, 8, 1];
+console.log(Math.max(...arr));
+
+const max = arr.reduce((a,b)=>a>b?a:b);
+console.log(max);
+
+
+console.log(Array.isArray(arr));
+console.log(Array.isArray(max));
+
+
+const result = fetch('https://course.acciojob.com/').then(res => res.json()).catch((e)=>console.log(e));
+
